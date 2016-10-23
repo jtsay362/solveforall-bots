4a6c01b0a2e749f406f932dca2ecab06fb61e372
Jeff Tsay <jctsay@aim.com>
2016-10-22 20:19:13 -0700

    Initial commit of skeleton code (from botkit example)

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+
+var path = require('path');
+
+var SOURCE_PATH = 'src/**/*.js';
+var OUTPUT_DIR = 'dist'
+
+var paths = {
+     es6: ['es6/**/*.js'],
+     es5: 'es5',
+     // Must be absolute or relative to source map
+     sourceRoot: path.join(__dirname, 'es6'),
+};
+
+
+gulp.task('babel', function () {
+  return gulp.src(SOURCE_PATH)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(OUTPUT_DIR));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(SOURCE_PATH, ['babel']);
+});
+
+gulp.task('default', ['babel']);
